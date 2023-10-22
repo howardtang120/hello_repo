1eca0c408cdf36a153177fdcd4eaaf1cf451c06c
Howard120 <122664285+howardtang120@users.noreply.github.com>
2023-10-23 00:02:06 +0800

    Update struct.c

--- C notes/struct.c
+++ C notes/struct.c
@@ -23,11 +23,11 @@ int main()
             .rating = 5,
             .color = "red"}};
 
-    printf("Outer struct:\n");
-    printf("index: %d\n", my_outer.index);
-    printf("name: %s\n", my_outer.name);
+    printf("Outer struct:\n");                             //Outer struct:
+    printf("index: %d\n", my_outer.index);                 //index: 1
+    printf("name: %s\n", my_outer.name);                   //name: example
     printf("\n\n");
-    printf("Inner struct:\n");
-    printf("rating: %d\n", my_outer.inner_struct.rating);
-    printf("color: %s\n", my_outer.inner_struct.color);
+    printf("Inner struct:\n");                              //Inner struct:
+    printf("rating: %d\n", my_outer.inner_struct.rating);   //rating: 5
+    printf("color: %s\n", my_outer.inner_struct.color);     //color: red
 }
